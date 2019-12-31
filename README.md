5938ec7a1f0ba456bb8f027c734824fad979a865
Mircea (Mark) Halmagiu <mircea@smartly.io>
2019-12-31 11:12:32 +0200

    initial working implementation

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
-# Hello world JavaScript action
+# Comment to PR action
 
-This action prints "Hello World" or "Hello" + the name of a person to greet to the log. To learn how this action was built, see "[Creating a JavaScript action](https://help.github.com/en/articles/creating-a-javascript-action)" in the GitHub Help documentation.
+This action will add a comment to a PR. If `exclusive` flag is not explicitly set to false, the comment will only be created if there is not already an identical comment present.
 
 ## Inputs
 
-### `who-to-greet`
+### `comment`
 
-**Required** The name of the person to greet. Default `"World"`.
+**Required** The text of the comment.
 
-## Outputs
+### `repo-token`
 
-### `time`
+**Required** The token to access the repo, same one as is available in the action.
 
-The time we greeted you.
+### `exclusive`
+
+**Optional** Do not comment if there is already an identical comment. Default true.
 
 ## Example usage
 
 ```yaml
-uses: actions/hello-world-javascript-action@master
+uses: smartlyio/comment-to-pr-action@v1
 with:
-  who-to-greet: 'Mona the Octocat'
+  repo-token: ${{ secrets.GITHUB_TOKEN }}
+  comment: 'The kittens have decreed that this PR is acceptable!'
 ```
--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function getAllComments(client, pullRequest, comment) {
+    try {
+      const allComments = await client.pulls.listReviews({
+            owner: pullRequest.owner,
+            repo: pullRequest.repo,
+            pull_number: pullRequest.number,
+        });
+      const returnComments = allComments.data.map(a => a.body);
+      return returnComments;
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 }
+
+async function addComment(client, pullRequest, comment) {
+    try {
+      await client.pulls.createReview({
+            owner: pullRequest.owner,
+            repo: pullRequest.repo,
+            pull_number: pullRequest.number,
+            body: comment,
+            event: 'COMMENT'
+        });
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+}
+
+async function run ()
+{
+    // Get client and context
+    const client = new github.GitHub(core.getInput('repo-token'));
+    const context = github.context;
+    const pullRequest = context.issue;
+    const comment = core.getInput('comment');
+    const exclusive = core.getInput('exclusive');
+
+    let allComments;
+    await getAllComments(client, pullRequest).then(function(result) { allComments = result;});
+
+    if (allComments.includes(comment) && exclusive) {
+        console.log("COMMENT ALREADY EXISTS.")
+        console.log("Not adding a second time")
+    } else {
+        addComment(client, pullRequest, comment);
+    }
+}
+
+run();
